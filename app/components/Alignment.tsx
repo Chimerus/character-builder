41cea588d8fe1e2594c3870f3eceff7e37d8c20b
Jason Chan <jlc370@gmail.com>
2024-09-26 13:41:53 -0400

    updated alignment to update app state

--- app/components/Alignment.tsx
+++ app/components/Alignment.tsx
@@ -1,14 +1,16 @@
 "use client";
 import React, { useState } from "react";
 
-const Alignment: any = () => {
+interface AlignProps {
+    onSelectItem: (item: string) => void;
+}
+
+
+function Alignment({onSelectItem}: AlignProps) {
     const lawChaos: string[] = ["Lawful", "Neutral", "Chaotic"]
     const goodEvil: string[] = ["Good", "Neutral", "Evil"]
-    interface AlignProps {
-        onClick: (event: React.MouseEvent) => void;
-    }
-
     const [selected, setSelected] = useState("");
+
 
     function getAlign(code: string, moral: string):string {
         let align = code + " " + moral;
@@ -19,13 +21,19 @@ const Alignment: any = () => {
         return align
     }
 
+    function updateState(alignment: string) {
+        setSelected(alignment)
+        // and update character state
+        onSelectItem(alignment)
+    }
+
     return (
         <div className="grid grid-rows-3 grid-flow-col gap-4">
             {lawChaos.map((code) => (
                 goodEvil.map((moral) => (
                     <div 
                         key={getAlign(code, moral)}
-                        onClick={()=>setSelected(getAlign(code, moral))}
+                        onClick={()=>updateState(getAlign(code, moral))}
                         className={getAlign(code, moral) == selected ? "alignm p-2 border border-indigo-600 active" : "alignm p-2 border border-indigo-600"}
                     >{getAlign(code, moral)} </div>
                 ))
